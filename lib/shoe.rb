9096cc60782f642687246c3c5548292f5ecf0adc
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-04-21 11:26:10 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,17 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+  attr_reader :cobble
+
+  def initialize(brand)
+    @brand = brand
+    @cobble = cobble
+  end
+
+  def cobble
+    if @condition == "old"
+      puts "Your shoe is as good as new!"
+      @condition = "new"
+    end
+  end
+
+end
